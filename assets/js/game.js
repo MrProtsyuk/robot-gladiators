7f3b268d5880af1b84df3744f1e472608d7f1fb4
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-01-04 16:06:27 -0800

    created functions in javascript

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log("our robots name is " + playerName);
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+//fight();
